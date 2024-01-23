8a34f7e5d14f8c5a3bb3d6172957479301f668e2
jbenitgu <=>
2024-01-23 11:51:42 -0500

    mejora de performance

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,21 @@ public class Main {
     public static void main(String[] args) {
        int data[] = {10,	5,	8,	100,	3,	9};
 
-       int temp;
 
+        System.out.println("Nueva Mejora de Performance");
         for(int i=0; i< data.length ;i++){
             System.out.print(data[i] + " ");
         }
+        int temp;
         for(int j=0;j<data.length-1;j++){
             for(int i=0; i< data.length -1;i++){
-                if(data[i] <	data[i+1]){
+                if(data[i] < data[i+1]){
                     temp = data[i];
                     data[i] = data[i+1];
                     data[i+1]  = temp;
                 }
             }
         }
-
         System.out.println();
         System.out.println();
 
