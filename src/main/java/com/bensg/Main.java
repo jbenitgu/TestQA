73bcdfe588f959dd9d21c6f459dba9d64933774b
jbenitgu <=>
2024-01-26 09:28:10 -0500

    FIX

--- src/main/java/com/bensg/Main.java
+++ src/main/java/com/bensg/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("RESTA: " + resultado);
 
 
-        System.out.println("Mejoras al codigo")
+        System.out.println("Mejoras al codigo");
 
 
     }
