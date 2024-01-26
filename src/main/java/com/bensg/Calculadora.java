2c5e0edba1d20a02ddcc7172d0ec3640c483652c
jbenitgu <=>
2024-01-25 19:52:38 -0500

    cambios

--- src/main/java/com/bensg/Calculadora.java
+++ src/main/java/com/bensg/Calculadora.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.bensg;
 
 public class Calculadora {
 
---
+++ src/main/java/com/bensg/Main.java
@@ -0,0 +1,16 @@
+package com.bensg;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo Ntt");
+
+        Calculadora myCal = new Calculadora();
+
+        int resultado = myCal.resta(9, 4);
+        System.out.println("RESTA: " + resultado);
+
+
+    }
+
+
+}
