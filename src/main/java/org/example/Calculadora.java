8728ac947b0ef7faa1bf4dabdaed845b19aa0d97
jbenitgu <=>
2024-01-25 19:05:36 -0500

    cambios

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Calculadora {
+
+    public int suma(int a, int b){
+        int res = a + b;
+        return res;
+    }
+
+    public int resta(int a, int b){
+        int res = a - b;
+        return res;
+    }
+    public int multiplicacion(int a, int b){
+        int res = a * b;
+        return res;
+    }
+}
---
+++ src/test/java/CalculadoraTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.example.Calculadora;
+import org.junit.Test;
+
+
+public class CalculadoraTest {
+    Calculadora cal = new Calculadora();
+
+    @Test
+    public void testSuma(){
+        System.out.println("Ejecutando test Suma");
+        assertEquals(8, cal.suma(3,5));
+        assertEquals(103, cal.suma(3,100));
+    }
+
+    @Test
+    public void testResta(){
+        System.out.println("Ejecutando test Resta");
+        assertEquals(2, cal.resta(5,3));
+    }
+
+    @Test
+    public void testMultiplicacion(){
+        System.out.println("Ejecutando test Multiplicacion");
+        assertEquals(8, cal.multiplicacion(5,3));
+    }
+}
