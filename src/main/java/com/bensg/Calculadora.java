d4bfa1eded1d3cc79ccfbb30487a43f602145888
jbenitgu <110509411+jbenitgu@users.noreply.github.com>
2025-01-29 10:03:51 -0500

    Update Calculadora.java

--- src/main/java/com/bensg/Calculadora.java
+++ src/main/java/com/bensg/Calculadora.java
@@ -3,13 +3,13 @@ package com.bensg;
 public class Calculadora {
 
     public int suma(int a, int b){
-        int res = a - b;
+        int res = a + b;
         return res;
     }
 
     public int resta(int a, int b){
         int res = a - b;
-        return res;
+        return res
     }
     public int multiplicacion(int a, int b){
         int res = a * b;
